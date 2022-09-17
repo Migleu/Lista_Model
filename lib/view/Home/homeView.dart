061aeccb27b291291842e1c9aeb77e8efa1e5dff
Miguel Silva <megamaante123@gmail.com>
2022-09-17 11:53:34 -0300

    17/09/22

---
+++ lib/view/Home/homeView.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:money_search/data/MoneyController.dart';
+import 'package:money_search/model/ListPersonModel.dart';
+import 'package:money_search/model/MoneyModel.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+/// instancia do modelo para receber as informações
+List<ListPersonModel> model = [];
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Lista de pessoas'),
+          centerTitle: true,
+          backgroundColor: Colors.lightGreen,
+        ),
+        body: FutureBuilder<List<ListPersonModel>>(
+          ///future: local onde informções serão buscadas
+          future: MoneyController().getListPerson(),
+          builder: (context, snapshot) {
+            /// validação de carregamento da conexão
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            /// validação de erro
+            if (snapshot.error == true) {
+              return SizedBox(
+                height: 300,
+                child: Text("Vazio"),
+              );
+            }
+            /// passando informações para o modelo criado
+            model = snapshot.data ?? model;
+            return ListView.builder(
+              itemCount: model.length,
+              itemBuilder: (context, index) {
+                 ListPersonModel item = model[index];
+                 
+                 return Text(item.name ?? "");
+              }
+              );
+          },
+        ));
+  }
+
+  Future<Null> refresh() async {
+    setState(() {});
+  }
+}
